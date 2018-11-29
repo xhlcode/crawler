33bfe1f4a0ee09075980d332b4054ae05fe90402
xhlcode <347606601@qq.com>
2018-11-29 14:05:17 +0800

    爬虫相关  添加jar包依赖   htmlunit Test Class

---
+++ src/main/java/cn/xhlcode/crawler/HttpUtils.java
@@ -0,0 +1,110 @@
+package cn.xhlcode.crawler;
+
+import com.alibaba.fastjson.JSONObject;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpUtils {
+
+    /** logger */
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpUtils.class);
+    /**
+     * 超时时间
+     */
+    private int timeout = 20000;
+
+    /**
+     * 等待异步JS 执行时间
+     */
+    private int waitForBackgroundJavaScript = 30000;
+
+
+
+    private static HttpUtils httpUtils;
+
+    private HttpUtils(){}
+
+    public static HttpUtils getInstance() {
+        if (httpUtils == null) {
+            httpUtils = new HttpUtils();
+        }
+        return httpUtils;
+    }
+    public String getHtmlPageResponse(String url) throws Exception {
+        final WebClient webClient= new WebClient(BrowserVersion.CHROME);
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+        webClient.getOptions().setActiveXNative(false);
+        webClient.getOptions().setCssEnabled(false);
+        webClient.getOptions().setJavaScriptEnabled(true);
+        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+
+        webClient.getOptions().setTimeout(timeout);
+        webClient.setJavaScriptTimeout(timeout);
+
+        HtmlPage page = null;
+        try {
+            page = webClient.getPage(url);
+        } catch (Exception ex) {
+            LOGGER.error("访问异常", JSONObject.toJSONString(ex));
+            throw  ex;
+        } finally {
+            webClient.close();
+        }
+
+        webClient.waitForBackgroundJavaScript(waitForBackgroundJavaScript);
+
+        return page.asXml();
+    }
+
+    private Document parseHtmlToDoc(String html) throws Exception {
+        return removeHtmlSpace(html);
+    }
+
+    /**
+     * 移除文档中 空格
+     * @param html
+     * @return
+     */
+    private Document removeHtmlSpace(String html) {
+        Document doc = Jsoup.parse(html);
+        String result = doc.html().replace("&nbsp;","");
+        return Jsoup.parse(result);
+    }
+
+
+    /**
+     * 获取页面文档Document 对象（等待JS异步执行）
+     * @param url
+     * @return
+     * @throws Exception
+     */
+    public Document getHtmlResponseAsDocument(String url) throws Exception {
+        return parseHtmlToDoc(getHtmlPageResponse(url));
+    }
+
+
+
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    public int getWaitForBackgroundJavaScript() {
+        return waitForBackgroundJavaScript;
+    }
+
+    public void setWaitForBackgroundJavaScript(int waitForBackgroundJavaScript) {
+        this.waitForBackgroundJavaScript = waitForBackgroundJavaScript;
+    }
+}
---
+++ src/test/java/cn/xhlcode/crawler/HtmlUtilTest.java
@@ -0,0 +1,94 @@
+package cn.xhlcode.crawler;
+
+import com.alibaba.fastjson.JSONObject;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebRequest;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URL;
+
+public class HtmlUtilTest {
+    /** logger */
+    private static final Logger LOGGER = LoggerFactory.getLogger(HtmlUtilTest.class);
+
+
+
+    private static final String qqMusicUrl = "https://c.y.qq.com/rsc/fcgi-bin/fcg_user_created_diss?hostuin=347606601&sin=0&size=32&r=1512031330762&g_tk=345719309&jsonpCallback=MusicJsonCallback3561589509370917&loginUin=347606601&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8¬ice=0&platform=yqq&needNewCode=0";
+
+    @Test
+    public void HtmlUtilDemo() {
+
+        // 新建一个模拟谷歌chrome 浏览器客户端对象
+        final WebClient webClient= new WebClient(BrowserVersion.CHROME);
+        // 当执行JS 执行出错时 是否抛出异常
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        // 当HTTP 的状态非 200 时是否抛出异常
+        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+        // 是否允许 ActiveXNative
+        webClient.getOptions().setActiveXNative(false);
+        // 是否允许CSS
+        webClient.getOptions().setCssEnabled(false);
+        webClient.getOptions().setJavaScriptEnabled(true);
+        // 设置 支持Ajax
+        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+
+
+        HtmlPage page = null;
+        try {
+           page =  webClient.getPage("https://c.y.qq.com/rsc/fcgi-bin/fcg_user_created_diss?hostuin=347606601&sin=0&size=32&r=1512031330762&g_tk=345719309&jsonpCallback=MusicJsonCallback3561589509370917&loginUin=347606601&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8¬ice=0&platform=yqq&needNewCode=0");
+        } catch (Exception ex) {
+            LOGGER.error(JSONObject.toJSONString(ex));
+        } finally {
+            webClient.close();
+        }
+        // 异步执行JS 需要时间  线程阻塞 30s  等待异步JS执行完成
+        webClient.waitForBackgroundJavaScript(30000);
+
+        String pageXml = page.asXml();
+
+        System.out.println(pageXml);
+    }
+
+
+
+
+    @Test
+    public void httpUtilsTest () {
+
+        HttpUtils httpUtils = HttpUtils.getInstance();
+        String result = "";
+        try {
+            result = httpUtils.getHtmlPageResponse("http://www.xhlcode.cn");
+        } catch (Exception ex) {
+            LOGGER.error(JSONObject.toJSONString(ex));
+        }
+        System.out.println(result);
+    }
+
+
+    /**
+     * 模拟请求头
+     */
+    @Test
+    public void htmlunitRequest() {
+        try {
+            String referer = "https://y.qq.com/portal/profile.html";
+
+            URL url = new URL(qqMusicUrl);
+            WebClient webClient = new WebClient(BrowserVersion.CHROME);
+            WebRequest request = new WebRequest(url);
+            request.setAdditionalHeader("Referer",referer);
+            HtmlPage page = webClient.getPage(request);
+            String pageXml = page.asXml();
+            System.out.println(pageXml);
+        } catch (Exception ex) {
+            LOGGER.info(JSONObject.toJSONString(ex));
+        }
+    }
+}
